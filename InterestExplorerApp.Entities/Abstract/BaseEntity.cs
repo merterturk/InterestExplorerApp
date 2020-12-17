6df0a1d51d97ebbf9438c41e12c658df5fe636fc
Mert <meerterturk@gmail.com>
2020-12-17 20:11:36 +0300

    First-commit

---
+++ InterestExplorerApp.Entities/Abstract/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestExplorerApp.Entities.Abstract
+{
+   public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime? UpdatedDate { get; set; }
+
+        public DateTime? DeletedDate { get; set; }
+
+    }
+}
---
+++ InterestExplorerApp/Areas/admin/Controllers/AnasayfaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterestExplorerApp.Areas.admin.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        // GET: admin/Anasayfa
+        public ActionResult Giris()
+        {
+            return View();
+        }
+    }
+}
